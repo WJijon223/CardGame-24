242df985c7bae9093339189f1191b8a1a5a9596d
WJijon223 <williamjijon223@gmail.com>
2025-02-28 22:32:06 -0500

    added image files

--- src/main/java/edu/farmingdale/cardgame24/MainController.java
+++ src/main/java/edu/farmingdale/cardgame24/MainController.java
@@ -3,7 +3,7 @@ package edu.farmingdale.cardgame24;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
