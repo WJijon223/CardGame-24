10ea67c08caf0b36bea0dfcdcebdb37c62267ea5
WJijon223 <williamjijon223@gmail.com>
2025-03-01 14:29:28 -0500

    added style.css file and updated fxml layout in Scenebuilder

--- src/main/java/edu/farmingdale/cardgame24/MainApplication.java
+++ src/main/java/edu/farmingdale/cardgame24/MainApplication.java
@@ -11,7 +11,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 435, 280);
+        scene.getStylesheets().add(MainApplication.class.getResource("style.css").toExternalForm());
         stage.setTitle("CardGame-24");
         stage.setScene(scene);
         stage.show();
--- src/main/java/edu/farmingdale/cardgame24/MainController.java
+++ src/main/java/edu/farmingdale/cardgame24/MainController.java
@@ -1,14 +1,6 @@
 package edu.farmingdale.cardgame24;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
