ca6315323a9a38c356d9d22a414fe397cbd7feb7
WJijon223 <williamjijon223@gmail.com>
2025-03-03 00:25:24 -0500

    finished editing GUI in SceneBuilder

--- src/main/java/edu/farmingdale/cardgame24/MainController.java
+++ src/main/java/edu/farmingdale/cardgame24/MainController.java
@@ -1,6 +1,58 @@
 package edu.farmingdale.cardgame24;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class MainController {
+
+    @FXML
+    private ImageView card1;
+
+    @FXML
+    private ImageView card2;
+
+    @FXML
+    private ImageView card3;
+
+    @FXML
+    private ImageView card4;
+
+    @FXML
+    private HBox cardHbox;
+
+    @FXML
+    private TextField expressionTextField;
+
+    @FXML
+    private Button findSolutionButton;
+
+    @FXML
+    private Button refreshButton;
+
+    @FXML
+    private Label solutionLabel;
+
+    @FXML
+    private Button verifyButton;
+
+    @FXML
+    void findSolutionAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refreshAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verifyAction(ActionEvent event) {
+
+    }
 
 }
